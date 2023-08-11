ce642f3c07a48b1d799a43256ef72b0893fbb247
Truong Bach Minh <86811396+Kisari@users.noreply.github.com>
2023-08-11 15:32:09 +0700

    Update user.route.js

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
+// generate possible route for user
+
 const router = express.Router();
 
 // router.get("/login", );
 
-module.exports = router;
+module.exports = userRouter;

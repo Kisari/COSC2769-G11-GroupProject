922df5d6d13d68ca0dab97205c596cc28e0f837c
Truong Bach Minh <86811396+Kisari@users.noreply.github.com>
2023-08-11 15:32:51 +0700

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const userRouter = require("./user.route");
 
+// use the router with prefix "api/v1/user"
+
 module.exports = (app) => {
   // app.use("/api/v1/user", userRouter);
 };

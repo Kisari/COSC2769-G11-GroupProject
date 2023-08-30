adaf15c7840434e400497fc091b8e72f22114d74
ToGiaHy <100851997+ToGiaHy@users.noreply.github.com>
2023-08-30 12:50:51 +0700

    category create

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,6 +1,18 @@
 const Category = require("../models/category.model");
 
 exports.createNewCategory = async(req,res) => {
-    const name = req.body.name;
+
+    let data = {
+        name : req.body.name,
+        description : req.body.description,
+        attributes : req.body.attributes,
+    }
+
+    data.attributes = JSON.parse(data.attributes); 
+
+    Category.create({data})
+}
+
+exports.getAllCategory = async(req,res) => {
     
 }

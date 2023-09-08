b739cf1109319666ee691e7c4611f96faf4189f0
Kisari <seikakukisari@gmail.com>
2023-09-08 15:05:19 +0700

    loading effect for login + signup

--- client/src/components/ui/SignUpForm.js
+++ client/src/components/ui/SignUpForm.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 import { signupUser } from "../../action/auth.js";
+import { useNavigate } from "react-router-dom";
 
 import Input from "./Input";
 import Loading from "./Loading";
 
 function SignUpForm({ type }) {
   const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleLogin = async (event) => {
+    setIsLoading(true);
     event.preventDefault();
     var payload = {
       email: event.target[0].value,
@@ -23,55 +27,64 @@ function SignUpForm({ type }) {
       payload = { ...payload, name: event.target.business.value };
     }
     await signupUser(payload).then((res) => {
-      console.log(res);
+      if (res) {
+        //navigate to login page
+        navigate("/login");
+      }
+      setIsLoading(true);
     });
-    console.log(payload);
   };
   return (
-    <form className="px-2 pt-4" onSubmit={(event) => handleLogin(event)}>
-      <Input
-        name={"email"}
-        type={"email"}
-        id={"email" + type}
-        placeholder={"Email@example.come"}
-      />
-      <Input
-        name={"phone"}
-        type={"number"}
-        id={"phone" + type}
-        placeholder={"Phone number"}
-      />
-      <Input
-        name={"password"}
-        type={showPassword ? "text" : "password"}
-        id={"password" + type}
-        placeholder={"Password"}
-        setShowPassword={setShowPassword}
-      />
-      {type === "customer" ? (
-        <Input
-          name={"address"}
-          type={"text"}
-          id={"address"}
-          placeholder={"Address"}
-        />
+    <>
+      {isLoading ? (
+        <Loading />
       ) : (
-        <Input
-          name={"business"}
-          type={"text"}
-          id={"business"}
-          placeholder={"Business name"}
-        />
+        <form className="px-2 pt-4" onSubmit={(event) => handleLogin(event)}>
+          <Input
+            name={"email"}
+            type={"email"}
+            id={"email" + type}
+            placeholder={"Email@example.come"}
+          />
+          <Input
+            name={"phone"}
+            type={"number"}
+            id={"phone" + type}
+            placeholder={"Phone number"}
+          />
+          <Input
+            name={"password"}
+            type={showPassword ? "text" : "password"}
+            id={"password" + type}
+            placeholder={"Password"}
+            setShowPassword={setShowPassword}
+          />
+          {type === "customer" ? (
+            <Input
+              name={"address"}
+              type={"text"}
+              id={"address"}
+              placeholder={"Address"}
+            />
+          ) : (
+            <Input
+              name={"business"}
+              type={"text"}
+              id={"business"}
+              placeholder={"Business name"}
+            />
+          )}
+          <div className="d-flex justify-content-end pt-3">
+            <button type="reset" className="btn btn-light btn-lg">
+              Reset
+            </button>
+            <button type="submit" className="btn btn-warning btn-lg ms-2">
+              Sign up
+            </button>
+          </div>
+        </form>
       )}
-      <div className="d-flex justify-content-end pt-3">
-        <button type="reset" className="btn btn-light btn-lg">
-          Reset
-        </button>
-        <button type="submit" className="btn btn-warning btn-lg ms-2">
-          Sign up
-        </button>
-      </div>
-    </form>
+    </>
   );
 }
 

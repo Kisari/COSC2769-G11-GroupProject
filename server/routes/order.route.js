e4f214a6242d296d1bbdb7867e8d22d611de2e10
xian-94 <s3957034@rmit.edu.vn>
2023-09-10 02:33:17 +0700

    done order

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -4,15 +4,17 @@ const orderRouter = express.Router();
 const {requireAuth} = require('../helpers/user.helper');
 
 // Check out
-orderRouter.get('/api/v1/checkout', requireAuth, orderController.checkout);
+orderRouter.post('/api/v1/checkout', requireAuth, orderController.checkout);
 // View
-orderRouter.get('/api/v1/customer/order');
-orderRouter.get('/api/v1/seller/order');
-orderRouter.get('/api/v1/customer/order/:id');
-orderRouter.get('/api/v1/seller/order/:id');
+orderRouter.get('/api/v1/customer/order', requireAuth, orderController.customerGetAll);
+orderRouter.get('/api/v1/seller/order', requireAuth, orderController.sellerGetAll);
+orderRouter.get('/api/v1/customer/order/:id', requireAuth, orderController.customerGet );
+orderRouter.get('/api/v1/seller/order/:id', requireAuth, orderController.sellerGet);
 // Update order status
-orderRouter.post('/api/v1/customer/order/detials/:id/accepted');
-orderRouter.post('/api/v1/customer/order/details/:id/rejected');
+orderRouter.put('/api/v1/customer/order/:orderId/details/:id/accepted', requireAuth, orderController.customerAccept);
+orderRouter.put('/api/v1/customer/order/:orderId/details/:id/rejected', requireAuth, orderController.customerReject);
+orderRouter.put('/api/v1/seller/order/:orderId/details/:id/shipped', requireAuth, orderController.sellerShipped);
+orderRouter.put('/api/v1/seller/order/:orderId/details/:id/canceled', requireAuth, orderController.sellerCanceled)
 
 
 

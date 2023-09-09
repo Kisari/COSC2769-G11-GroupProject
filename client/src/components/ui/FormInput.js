a479db8d83eea4c08d5e11a5292fc8f669ffca02
Kisari <seikakukisari@gmail.com>
2023-09-09 17:35:55 +0700

    commit

--- client/src/components/ui/FormInput.js
+++ client/src/components/ui/FormInput.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 
-const FormInput = ({ label, placeholder, type, helperText }) => {
+const FormInput = ({ label, placeholder, type, helperText, name }) => {
   return (
     <Form.Group className="mb-3" controlId="formBasicEmail">
       <Form.Label>{label}</Form.Label>
-      <Form.Control type={type} placeholder={placeholder} required />
+      <Form.Control
+        type={type}
+        placeholder={placeholder}
+        required
+        name={name}
+        min="1"
+      />
       {helperText && <Form.Text className="text-muted">helperText</Form.Text>}
     </Form.Group>
   );

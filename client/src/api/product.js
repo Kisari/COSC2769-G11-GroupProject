984c44bd5cc583e910f54a94474dacf18596d369
xian-94 <s3957034@rmit.edu.vn>
2023-09-10 12:53:47 +0700

    hotfix upload image

--- client/src/api/product.js
+++ client/src/api/product.js
@@ -9,10 +9,18 @@ export const getProductByID = (id) =>
   axios.get(`${backend_url}/api/v1/product/${id}`);
 
 export const createProduct = (formData) =>
-  axios.post(`${backend_url}/api/v1/product`, formData);
+  axios.post(`${backend_url}/api/v1/product`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
 
 export const deleteProduct = (id) =>
   axios.delete(`${backend_url}/api/v1/product/${id}`);
 
 export const updateProduct = (id, formData) =>
-  axios.put(`${backend_url}/api/v1/product/${id}`, formData);
+  axios.put(`${backend_url}/api/v1/product/${id}`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
